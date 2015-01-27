ff5a4f69336d135a00a79055b5b45f291ec1b32a
Simon <simon@bitcartel.com>
2015-01-26 18:04:46 -0800

    Refactored and persist CSMessagePart to database.

---
+++ core/src/main/java/org/coinspark/wallet/CSMessagePart.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015 Coin Sciences Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.coinspark.wallet;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * CSMessagePart represents a messaging attachment which is persisted to database.
+ */
+@DatabaseTable(tableName = "message_parts")
+public class CSMessagePart {
+
+    public static final String GENERATED_ID_FIELD_NAME = "auto_id";
+    public static final String MESSAGE_ID_FIELD_NAME = "message_id";
+    public static final String PART_ID_FIELD_NAME = "part_id";
+    public static final String MIME_TYPE_FIELD_NAME = "mimetype";
+    public static final String FILE_NAME_FIELD_NAME = "filename";
+    public static final String CONTENT_SIZE_FIELD_NAME = "size";
+    public static final String CONTENT_FIELD_NAME = "content";
+
+    // Auto-generated id sequence needed by Dao
+    @DatabaseField(generatedId = true, columnName=GENERATED_ID_FIELD_NAME)
+    public long id;
+
+    @DatabaseField(foreign=true, columnName=MESSAGE_ID_FIELD_NAME, canBeNull = false, uniqueCombo=true)
+    public CSMessage message;
+
+    @DatabaseField(columnName=PART_ID_FIELD_NAME, canBeNull = false, uniqueCombo=true)
+    public int partID;
+    
+    @DatabaseField(columnName = MIME_TYPE_FIELD_NAME, canBeNull = true)
+    public String mimeType;
+
+    @DatabaseField(columnName = FILE_NAME_FIELD_NAME, canBeNull = true)
+    public String fileName;
+
+    // Is this required or can it be derived from Blob?
+    @DatabaseField(columnName = CONTENT_SIZE_FIELD_NAME, canBeNull = false)
+    public int contentSize;
+
+    // columnDefinition overrides canBeNull=false so must explicitly specify NOT NULL
+    @DatabaseField(columnName = CONTENT_FIELD_NAME, columnDefinition = "LONGBLOB not null", dataType=DataType.BYTE_ARRAY)
+    public byte[] content;
+
+    CSMessagePart(int partID, String MimeType, String FileName, int ContentSize, byte[] Content) {
+	this.partID = partID;
+	mimeType = MimeType;
+	fileName = FileName;
+	contentSize = ContentSize;
+	content = Content;
+    }
+
+    CSMessagePart() {
+    }
+}
